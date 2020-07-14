218844e4ce1c7e233cddd1c3c9341c5a4608bab4
szymon-gawel <szymon.gawel@op.pl>
2020-07-14 14:43:01 +0200

    Adding EntityFramework and first Migrations

---
+++ MyStore/MyStore.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using MyStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
